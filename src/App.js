310df2bafb22f22402ae05c4138c3e46bfe964b1
Keegan Brown <brown.keegan@gmail.com>
2017-02-16 20:17:39 -0600

    Just a box

--- src/App.js
+++ src/App.js
@@ -33,9 +33,9 @@ let birdVS = require('./shaders/bird.vert.glsl');
 
 export default class App {
   constructor() {
-    this._bind('_render', '_handleResize');
-    this._setup3D();
-    this._createScene();
+    this.bindHandler('_render', '_handleResize');
+    this.setup3D();
+    this.createScene();
 
     window.addEventListener('resize', this._handleResize.bind(this));
   }
@@ -49,33 +49,32 @@ export default class App {
     requestAnimationFrame(loop);
   }
 
-  _bind(...methods) {
+  bindHandler(...methods) {
     methods.forEach((method) => this[method] = this[method].bind(this));
   }
 
-  _setup3D() {
-    renderer = this._renderer = new THREE.WebGLRenderer({antialias: true});
+  setup3D() {
+    renderer = this.renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(1);
     document.body.appendChild(renderer.domElement);
 
-    this._setupScene();
-    this._setupListeners();
-    this.initComputeRenderer();
-    this.initBirds();
+    this.setupScene();
+    this.setupControls();
+    this.setupContent();
   }
 
-  _setupScene() {
-    scene = this._scene = new THREE.Scene();
-    this._scene.fog = new THREE.Fog( 0xffffff, 100, 1000 );
-    this._camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, NEAR, FAR);
-    camera = this._camera;
+  setupScene() {
+    scene = this.scene = new THREE.Scene();
+    this.scene.fog = new THREE.Fog( 0xffffff, 100, 1000 );
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, NEAR, FAR);
+    camera = this.camera;
     camera.position.y = (100);
     camera.position.z = (500);
   }
 
-  _setupListeners() {
-    controls = new THREE.TrackballControls( this._camera );
+  setupControls() {
+    controls = new THREE.TrackballControls( this.camera );
     controls.rotateSpeed = 1.0;
     controls.zoomSpeed = 1.2;
     controls.panSpeed = 0.8;
@@ -88,131 +87,28 @@ export default class App {
       console.log('controls')
       this._render();
     } );
-
   }
 
-  _createScene() {
-    scene = this._scene;
+  setupContent() {
+    let material = new THREE.MeshPhongMaterial( { color: 0xdddddd, specular: 0x009900, shininess: 0, shading: THREE.SmoothShading,transparent: true } );
+    let box = new THREE.Mesh( new THREE.BoxGeometry( 100, 100, 100, 4, 4, 4 ), material );
+    box.position.set( -200, 0, 0 );
+    this.scene.add( box );
+  }
+
+  createScene() {
+    scene = this.scene;
 
     var grid = new THREE.GridHelper(1000, 5, 0x333333, 0x333333);
     scene.add(grid);
   }
 
-  initComputeRenderer() {
-    gpuCompute = new GPUComputationRenderer( WIDTH, WIDTH, renderer );
-    var dtPosition = gpuCompute.createTexture();
-    var dtVelocity = gpuCompute.createTexture();
-    this.fillPositionTexture( dtPosition );
-    this.fillVelocityTexture( dtVelocity );
-
-    velocityVariable = gpuCompute.addVariable(
-      "textureVelocity",
-      velocityTexture,
-      dtVelocity
-    );
-
-    positionVariable = gpuCompute.addVariable(
-      "texturePosition",
-      positionTexture,
-      dtPosition
-    );
-
-    gpuCompute.setVariableDependencies( velocityVariable, [ positionVariable, velocityVariable ] );
-    gpuCompute.setVariableDependencies( positionVariable, [ positionVariable, velocityVariable ] );
-    positionUniforms = positionVariable.material.uniforms;
-    velocityUniforms = velocityVariable.material.uniforms;
-    positionUniforms.time = { value: 0.0 };
-    positionUniforms.delta = { value: 0.0 };
-    velocityUniforms.time = { value: 1.0 };
-    velocityUniforms.delta = { value: 0.0 };
-    velocityUniforms.testing = { value: 1.0 };
-    velocityUniforms.seperationDistance = { value: 1.0 };
-    velocityUniforms.alignmentDistance = { value: 1.0 };
-    velocityUniforms.cohesionDistance = { value: 1.0 };
-    velocityUniforms.freedomFactor = { value: 1.0 };
-    velocityUniforms.predator = { value: new THREE.Vector3() };
-    velocityVariable.material.defines.BOUNDS = BOUNDS.toFixed( 2 );
-    velocityVariable.wrapS = THREE.RepeatWrapping;
-    velocityVariable.wrapT = THREE.RepeatWrapping;
-    positionVariable.wrapS = THREE.RepeatWrapping;
-    positionVariable.wrapT = THREE.RepeatWrapping;
-    var error = gpuCompute.init();
-    if ( error !== null ) {
-      console.error( error );
-    }
-  }
-
-  initBirds() {
-    var geometry = new THREE.BirdGeometry();
-    const scene = this._scene;
-    birdUniforms = {
-      color: { value: new THREE.Color( 0xff2200 ) },
-      texturePosition: { value: null },
-      textureVelocity: { value: null },
-      time: { value: 1.0 },
-      delta: { value: 0.0 }
-    };
-    var material = new THREE.ShaderMaterial( {
-      uniforms:       birdUniforms,
-      vertexShader:   birdVS,
-      fragmentShader: birdFS,
-      side: THREE.DoubleSide
-    });
-    birdMesh = new THREE.Mesh( geometry, material );
-    birdMesh.rotation.y = Math.PI / 2;
-    birdMesh.matrixAutoUpdate = false;
-    birdMesh.updateMatrix();
-    scene.add(birdMesh);
-  }
-
-  fillPositionTexture(texture) {
-    var theArray = texture.image.data;
-    for ( var k = 0, kl = theArray.length; k < kl; k += 4 ) {
-      var x = Math.random() * BOUNDS - BOUNDS_HALF;
-      var y = Math.random() * BOUNDS - BOUNDS_HALF;
-      var z = Math.random() * BOUNDS - BOUNDS_HALF;
-      theArray[ k + 0 ] = x;
-      theArray[ k + 1 ] = y;
-      theArray[ k + 2 ] = z;
-      theArray[ k + 3 ] = 1;
-    }
-  }
-  fillVelocityTexture(texture) {
-    var theArray = texture.image.data;
-    for ( var k = 0, kl = theArray.length; k < kl; k += 4 ) {
-      var x = Math.random() - 0.5;
-      var y = Math.random() - 0.5;
-      var z = Math.random() - 0.5;
-      theArray[ k + 0 ] = x * 10;
-      theArray[ k + 1 ] = y * 10;
-      theArray[ k + 2 ] = z * 10;
-      theArray[ k + 3 ] = 1;
-    }
-  }
-
   _render(timestamp) {
-    const scene = this._scene;
-    const camera = this._camera;
-    const renderer = this._renderer;
+    const scene = this.scene;
+    const camera = this.camera;
+    const renderer = this.renderer;
 
     renderer.render(scene, camera);
-
-    var now = performance.now();
-    var delta = (now - last) / 1000;
-    if (delta > 1) delta = 1;
-    last = now;
-    positionUniforms.time.value = now;
-    positionUniforms.delta.value = delta;
-    velocityUniforms.time.value = now;
-    velocityUniforms.delta.value = delta;
-    birdUniforms.time.value = now;
-    birdUniforms.delta.value = delta;
-    velocityUniforms.predator.value.set( 0.5 * mouseX / windowHalfX, - 0.5 * mouseY / windowHalfY, 0 );
-    mouseX = 10000;
-    mouseY = 10000;
-    gpuCompute.compute();
-    birdUniforms.texturePosition.value = gpuCompute.getCurrentRenderTarget( positionVariable ).texture;
-    birdUniforms.textureVelocity.value = gpuCompute.getCurrentRenderTarget( velocityVariable ).texture;
   }
 
   animate() {
@@ -221,8 +117,8 @@ export default class App {
   }
 
   _handleResize(event) {
-    const renderer = this._renderer;
-    const camera = this._camera;
+    const renderer = this.renderer;
+    const camera = this.camera;
 
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
